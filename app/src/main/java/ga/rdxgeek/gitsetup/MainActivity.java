552baff82279935982057382ab91997f9e0c657f
duke7able <mehtashreyan041.sm@gmail.com>
2016-12-13 22:25:05 +0530

    Initial commit...

---
+++ app/src/main/java/ga/rdxgeek/gitsetup/MainActivity.java
@@ -0,0 +1,18 @@
+package ga.rdxgeek.gitsetup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void testmethod(){
+        int i = 1+1 ;
+        
+    }
+}

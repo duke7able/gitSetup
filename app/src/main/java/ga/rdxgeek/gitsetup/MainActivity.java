47e1ddd2db047c75efc19773f8c2110988b25d1a
duke7able <mehtashreyan041.sm@gmail.com>
2016-12-13 23:10:07 +0530

    first push , modified mainactivity and added new file test

--- app/src/main/java/ga/rdxgeek/gitsetup/MainActivity.java
+++ app/src/main/java/ga/rdxgeek/gitsetup/MainActivity.java
@@ -13,6 +13,9 @@ public class MainActivity extends AppCompatActivity {
 
     public void testmethod(){
         int i = 1+1 ;
-        
+
+        int ii = 2+2;
+
+        int iii = 3+3;
     }
 }
---
+++ app/src/main/java/ga/rdxgeek/gitsetup/test.java
@@ -0,0 +1,8 @@
+package ga.rdxgeek.gitsetup;
+
+/**
+ * Created by Shreyan on 12/13/2016.
+ */
+
+public class test {
+}
